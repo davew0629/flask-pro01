f73683b250ecbf44280a70512fae4cbc4765030a
davew0629 <15509361@qq.com>
2018-12-10 21:24:23 +0800

    添加项目初始化配置

--- manage.py
+++ manage.py
@@ -1,6 +1,26 @@
 from flask import Flask, request
+from flask.ext.sqlalchemy import SQLAlchemy
+
+class Config(object):
+    # 项目的配置
+    DEBUG = True
+    # 为数据库添加配置
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information27"
+    SQLALCHEMY_MODIFICATIONS = False
+
+
+
+
+
 
 app = Flask(__name__)
+# 加载配置
+app.config.from_object(Config)
+
+
+# 初始化数据库
+db = SQLAlchemy(app)
+
 
 
 @app.route('/')
