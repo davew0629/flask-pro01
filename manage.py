29d292ec1502b781870290366784c9d7f8bd4450
davew0629 <15509361@qq.com>
2018-12-10 21:48:04 +0800

    添加项目初始化配置

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 from flask.ext.sqlalchemy import SQLAlchemy
+from redis import StrictRedis
+from flask.ext.wtf import CSRFProtect
 
 class Config(object):
     # 项目的配置
@@ -8,6 +10,9 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information27"
     SQLALCHEMY_MODIFICATIONS = False
 
+    # redis 配置
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
 
 
 
@@ -20,6 +25,18 @@ app.config.from_object(Config)
 
 # 初始化数据库
 db = SQLAlchemy(app)
+
+# 初始化redis存储对象
+redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+
+# 开启当前项目CSRF保护,只做服务器验证
+CSRFProtect(app)
+
+
+
+
+
+
 
 
 
